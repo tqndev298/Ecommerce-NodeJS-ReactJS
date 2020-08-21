8aa48c4d08404f2b81a0f7c2ceaedcb7a6794521
I am TQN <uznaruto298@gmail.com>
2020-08-21 11:43:16 +0700

    React Hooks - 7

--- ecommerce-server/app.js
+++ ecommerce-server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const expressValidator = require("express-validator");
@@ -28,6 +29,7 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 
 //routes middleware
 app.use("/api", authRoutes);
