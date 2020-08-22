2961bfeebaec654e066e49cf7652ce13854c114b
I am TQN <uznaruto298@gmail.com>
2020-08-22 15:57:05 +0700

    Categories and Products - 1

--- ecommerce-client/src/user/Signin.js
+++ ecommerce-client/src/user/Signin.js
@@ -82,6 +82,9 @@ const Signin = () => {
         return <Redirect to="/user/dashboard" />;
       }
     }
+    if (isAuthenticated()) {
+      return <Redirect to="/" />;
+    }
   };
   return (
     <Layout
