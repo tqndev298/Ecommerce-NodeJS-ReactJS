9a1bb2d6fd5a74dcbb952b8f588cfd5f1cf4a73f
I am TQN <uznaruto298@gmail.com>
2020-08-24 00:55:11 +0700

    Product Page with Related Products - 1

--- ecommerce-client/src/core/apiCore.js
+++ ecommerce-client/src/core/apiCore.js
@@ -1,11 +1,52 @@
 import { API } from "../config";
+import queryString from "query-string";
 
 export const getProducts = (sortBy) => {
-    return fetch(`${API}/products?sortBy=${sortBy}&order=desc&limit=6`, {
-      method: "GET",
+  return fetch(`${API}/products?sortBy=${sortBy}&order=desc&limit=6`, {
+    method: "GET",
+  })
+    .then((res) => {
+      return res.json();
     })
-      .then((res) => {
-        return res.json();
-      })
-      .catch((err) => console.log(err));
-  };
+    .catch((err) => console.log(err));
+};
+
+export const getCategories = () => {
+  return fetch(`${API}/categories`, {
+    method: "GET",
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const getFilteredProducts = (skip, limit, filters = {}) => {
+  const data = { limit, skip, filters };
+  return fetch(`${API}/products/by/search`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export const list = (params) => {
+  const query = queryString.stringify(params);
+  console.log("query", query);
+  return fetch(`${API}/products/search?${query}`, {
+    method: "GET",
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => console.log(err));
+};
