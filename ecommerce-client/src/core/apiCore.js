5abf4a4ab616a49b3ae54cca29610f5cc530a33c
I am TQN <uznaruto298@gmail.com>
2020-08-24 11:45:41 +0700

    Cart CRUD with LocalStorage - 1

--- ecommerce-client/src/core/apiCore.js
+++ ecommerce-client/src/core/apiCore.js
@@ -50,3 +50,23 @@ export const list = (params) => {
     })
     .catch((err) => console.log(err));
 };
+
+export const read = (productId) => {
+  return fetch(`${API}/product/${productId}`, {
+    method: "GET",
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const listRelated = (productId) => {
+  return fetch(`${API}/products/related/${productId}`, {
+    method: "GET",
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => console.log(err));
+};
