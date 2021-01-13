c43579b42e4a3206efcbb22053ed1a25a26bde57
My name is TQN <uznaruto298@gmail.com>
2021-01-13 21:56:12 +0700

    fix

--- ecommerce-server/controllers/auth.js
+++ ecommerce-server/controllers/auth.js
@@ -2,7 +2,6 @@ const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 const expressJwt = require("express-jwt");
 const { errorHandler } = require("../helpers/dbErrorHandlers");
-const user = require("../models/user");
 
 exports.signup = (req, res) => {
   console.log("req.body", req.body);
